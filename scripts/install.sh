0a1ae3fe678ced7c7b81998e0e7a617e24611cd7
riyagarasangi <157198559+riyagarasangi@users.noreply.github.com>
2025-05-06 07:24:59 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,6 +5,6 @@ echo "Running install.sh"
 cd /home/ec2-user/mywebapp
 
 # Install Node.js dependencies
-npm install
+npm install || { echo "npm install failed"; exit 1; }
 
 echo "Dependencies installed"
